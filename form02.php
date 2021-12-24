74ca6a4b9a42bba7f11c5f32412312b44bc4b201
apisitgo <apisit.sa@go.buu.ac.th>
2021-12-24 17:51:52 +0700

    form1

---
+++ form02.php
@@ -0,0 +1,23 @@
+aaaaaaaaaaaaaaaaaaaa
+z<!DOCTYPE html>
+z<html>
+z<head>
+z<title>table</title>
+z</head>
+z<body>
+z      <table border="1px" height="20">
+      <tr>
+            <td>4</td>
+            <td>4</td>
+            <td>4</td>
+      </tr>
+      <tr>
+            <td>4</td>
+            <td>4</td>
+            <td>4</td>
+
+      </tr>
+    
+      </table>
+</body>
+</html>
